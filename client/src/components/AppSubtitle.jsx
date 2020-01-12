04c857cdf70830b7d4bf85c1ac772f7e99da4215
klevcsoo <kevelajsz@outlook.com>
2020-01-12 18:18:25 +0100

    Added noMargin prop

--- client/src/components/AppSubtitle.jsx
+++ client/src/components/AppSubtitle.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-const AppSubtitle = ({ text }) => {
+const AppSubtitle = ({ text, noMargin }) => {
   return (
     <h1 style={{
       fontSize: 20,
       fontWeight: 300,
       opacity: 0.8,
-      textAlign: 'center'
+      textAlign: 'center',
+      margin: noMargin ? 0 : 10
     }}>{text}</h1>
   )
 }
