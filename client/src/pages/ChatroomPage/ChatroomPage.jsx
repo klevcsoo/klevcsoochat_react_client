ebd9ddc0808c22260c35c43c58a4361ad74289be
klevcsoo <kevelajsz@outlook.com>
2020-02-18 18:45:18 +0100

    Cleaned up ChatroomPage code

--- client/src/pages/ChatroomPage/ChatroomPage.jsx
+++ client/src/pages/ChatroomPage/ChatroomPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react'
+import './ChatroomPage.css'
 import { firebaseHandler } from '../../Utils'
 import { useRouteMatch } from 'react-router-dom'
 import { appColours } from '../../Contants'
@@ -16,7 +17,6 @@ const ChatroomPage = () => {
   const bottomRef = useRef(null)
   const inputRef = useRef(null)
   const [ loading, setLoading ] = useState(true)
-  const [ sending, setSending ] = useState(false)
   const [ messages, setMessages ] = useState([])
   const [ currentMsg, setCurrentMsg ] = useState('')
 
@@ -31,20 +31,8 @@ const ChatroomPage = () => {
 
   return (
     <div>
-      <div style={{
-        position: 'fixed',
-        top: 0, left: 0,
-        zIndex: 110,
-        width: '100vw', height: 44,
-        background: appColours.FOREGROUND,
-        borderBottomLeftRadius: 20,
-        borderBottomRightRadius: 20,
-        display: 'grid',
-        gridTemplateRows: 'auto min-content auto'
-      }}>
-        <div style={{ gridRow: '2 / span 1' }}>
-          <h1 className="app-subtitle" style={{ margin: 0 }}>Chat::<b>{roomId}</b></h1>
-        </div>
+      <div className="chatroom-header">
+        <div><h1 className="app-subtitle" style={{ margin: 0 }}>Chat::<b>{roomId}</b></h1></div>
       </div>
       <div style={{ height: 50 }}></div>
       {loading ? (
@@ -63,35 +51,20 @@ const ChatroomPage = () => {
         </div>
       )}
       <div style={{ height: 100 }} ref={bottomRef}></div>
-      <div style={{
-        position: 'fixed',
-        bottom: 0, left: 0,
-        zIndex: 100,
-        width: '100vw', height: 50,
-        paddingBottom: 34,
-        background: appColours.FOREGROUND,
-        borderTopLeftRadius: 10,
-        borderTopRightRadius: 10,
-        display: 'grid',
-        gridTemplateColumns: 'auto min-content'
-      }}>
+      <div className="chatroom-message-composer">
         <AppInput inChat placeholder="Aa" defaultValue={currentMsg} onChange={(text) => {
           setCurrentMsg(text)
         }} reference={inputRef} />
-        {sending ? <LoadingSpinner /> : (
-          <AppSendButton disabled={currentMsg.length === 0} onClick={() => {
-            setSending(true)
-            if (inputRef.current) inputRef.current.focus()
-            firebaseHandler.sendMessage(roomId, currentMsg, () => {
-              console.log('Sent')
-              setCurrentMsg('')
-              setSending(false)
-              if (bottomRef.current) bottomRef.current.scrollIntoView({ behaivor: 'smooth' })
-            }, (err) => {
-              console.log(err)
-            })
-          }} />
-        )}
+        <AppSendButton disabled={currentMsg.length === 0} onClick={() => {
+          if (inputRef.current) inputRef.current.focus()
+          firebaseHandler.sendMessage(roomId, currentMsg, () => {
+            console.log('Sent')
+            setCurrentMsg('')
+            if (bottomRef.current) bottomRef.current.scrollIntoView({ behaivor: 'smooth' })
+          }, (err) => {
+            console.log(err)
+          })
+        }} />
       </div>
     </div>
   )
