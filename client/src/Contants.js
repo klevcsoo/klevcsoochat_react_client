0da2dad6a02572ecc3f0b898eaac99caf65f63ab
klevcsoo <kevelajsz@outlook.com>
2020-03-11 20:37:57 +0100

    Added new page routes

--- client/src/Contants.js
+++ client/src/Contants.js
@@ -4,11 +4,12 @@ export const appColours = {
   TEXT: 'var(--colour-text)',
   PRIMARY: 'var(--colour-primary)',
   PRIMARY_SHADOW: 'var(--primary-shadow)',
-  MONO_SHADOW: 'var(--mono-shadow)'
 }
 
 export const routes = {
   HOME: '/',
+  CREATE_CHATROOM: '/create-chat',
   CHATROOM: '/chat/:chatroom_id',
-  SETTINGS: '/settings'
+  CHATROOM_SETTINGS: '/chat/:chatroom_id/settings',
+  USER_SETTINGS: '/account-settings'
 }
