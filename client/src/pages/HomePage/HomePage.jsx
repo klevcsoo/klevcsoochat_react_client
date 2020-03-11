fcaebae2b77d32f6b739c7347cd81a86b1aa592a
klevcsoo <kevelajsz@outlook.com>
2020-03-11 20:38:13 +0100

    Reworked home page

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,76 +1,69 @@
 import React, { useState, useEffect } from 'react'
 import './HomePage.css'
 import { useHistory } from 'react-router-dom'
-import { firebaseHandler } from '../../Utils'
+import { firebaseHandler, useAuthUser } from '../../Utils'
 import { routes } from '../../Contants'
-import cookie from 'react-cookies'
 
 // Components
-import AppButton from '../../components/AppButton/AppButton'
-import AppInput from '../../components/AppInput/AppInput'
-import LoadingSpinner from '../../components/LoadingSpinner'
 import AppLogo from '../../components/AppLogo'
+import AppInput from '../../components/AppInput/AppInput'
+import AppButton from '../../components/AppButton/AppButton'
+import LoadingSpinner from '../../components/LoadingSpinner'
 
 const HomePage = () => {
   const history = useHistory()
-  const [ username, setUsername ] = useState('')
+  const redirectRoomId = history.location.search.replace('?chat=', '')
+  const [ user, userLoading ] = useAuthUser()
+  const [ email, setEmail ] = useState('')
+  const [ pass, setPass ] = useState('')
   const [ roomId, setRoomId ] = useState('')
-  const [ creating, setCreating ] = useState(false)
 
-  const joinChatroom = () => {
-    saveUsername()
-    history.push(routes.HOME.concat(`chat/${roomId}`))
+  console.log(redirectRoomId)
+
+  const attemptJoinChat = () => {
+    if (!roomId) history.push(routes.CREATE_CHATROOM)
+    else history.push(routes.HOME.concat(`chat/${roomId}`))
   }
 
-  const createChatroom = () => {
-    setCreating(true)
-    saveUsername()
-    firebaseHandler.createChatroom((id) => {
-      history.push(routes.HOME.concat(`chat/${id}`))
-    }, (err) => {
-      alert(err)
-      setCreating(false)
-    })
-  }
-
-  const saveUsername = () => {
-    if (!username) cookie.remove('username')
-    else cookie.save('username', username)
-
-    console.log('Username saved:', cookie.load('username'))
+  const attemptLogin = () => {
+    if (!email || !pass) alert('Hiányzó adatok!')
+    else firebaseHandler.login(email, pass)
   }
 
   useEffect(() => {
-    let cachedUsername = cookie.load('username')
-    if (!!cachedUsername) setUsername(cachedUsername)
-  }, [])
+    if (!!redirectRoomId && !!user) {
+      history.push(routes.HOME.concat(`chat/${redirectRoomId}`))
+    }
+  }, [ user, redirectRoomId, history ])
 
   return (
     <div>
       <div className="home-logo"><AppLogo/></div>
       <div className="home-content-container">
-        <div><div>
-          <AppInput placeholder="Felhasználónév" defaultValue={username} onChange={(text) => {
-            setUsername(text)
-          }} />
-          {creating ? <LoadingSpinner /> : <AppButton text="Szoba létrehozása" onClick={() => {
-            if (!username) alert('Nincs megadva felhasználónév')
-            else createChatroom()
-          }}  />}
-          <div className="home-option-separator"></div>
-        </div></div>
-        <div>
-          <AppInput placeholder="Felhasználónév" defaultValue={username} onChange={(text) => {
-            setUsername(text)
-          }} />
-          <AppInput placeholder="Chat azonosító" defaultValue={roomId} onChange={(text) => {
-            setRoomId(text)
-          }}  />
-          <AppButton text="Csatlakozás a szobához" onClick={() => {
-            if (!username) alert('Nincs megadva felhasználónév')
-            else if (!!roomId) joinChatroom()
-          }} />
-        </div>
+        {userLoading ? <LoadingSpinner /> : !!user ? (
+          <React.Fragment>
+            <h1 className="app-subtitle" onClick={() => firebaseHandler.logout()}>
+              <span style={{ fontSize: 30 }}>Bejelentkezve!</span><br/>
+              {user.email}<br/>
+              {user.displayName}
+            </h1>
+            <div className="home-option-separator"></div>
+            <div>
+            <AppInput placeholder="Szoba azonosító" onChange={(text) => setRoomId(text)}
+            onSubmit={() => attemptJoinChat()} />
+            <AppButton text={!!roomId ? 'Csatlakozás' : 'Szoba létrehozása'}
+            onClick={() => attemptJoinChat()} />
+          </div>
+          </React.Fragment>
+        ) : (
+          <div>
+            <AppInput placeholder="E-mail cím" onChange={(text) => setEmail(text)}
+            onSubmit={() => attemptLogin()} />
+            <AppInput placeholder="Jelszó" pass onChange={(text) => setPass(text)}
+            onSubmit={() => attemptLogin()} />
+            <AppButton text="Bejelentkezés" onClick={() => attemptLogin()} />
+          </div>
+        )}
       </div>
     </div>
   )
