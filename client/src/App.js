ade4d31898d416d24d467ecfd8e8dbff4310800a
klevcsoo <kevelajsz@outlook.com>
2020-03-12 18:18:22 +0100

    Added CreateChatroomPage

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import HomePage from './pages/HomePage/HomePage'
 import NotFoundPage from './pages/NotFoundPage'
 import ChatroomPage from './pages/ChatroomPage/ChatroomPage'
 import UserSettingsPage from './pages/UserSettingsPage/UserSettingsPage'
+import CreateChatroomPage from './pages/CreateChatroomPage/CreateChatroomPage'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route exact path={routes.HOME} component={HomePage} />
           <Route path={routes.USER_SETTINGS} component={UserSettingsPage} />
           <Route path={routes.CHATROOM} component={ChatroomPage} />
+          <Route path={routes.CREATE_CHATROOM} component={CreateChatroomPage} />
           <Route path="*" component={NotFoundPage} />
         </Switch>
       </BrowserRouter>
