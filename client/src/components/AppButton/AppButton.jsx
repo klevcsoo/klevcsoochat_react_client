7d8902c8ba496309c60d191af886678d79161392
klevcsoo <kevelajsz@outlook.com>
2020-03-12 17:00:36 +0100

    Added loading property

--- client/src/components/AppButton/AppButton.jsx
+++ client/src/components/AppButton/AppButton.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import './AppButton.css'
 
-const AppButton = ({ text, onClick }) => {
+const AppButton = ({ text, onClick, loading }) => {
   const handleClick = () => {
+    if (loading) return
     setTimeout(() => onClick(), 200)
   }
 
   return (
-    <div className="app-button" onClick={handleClick}>
+    <div className={`app-button${loading ? ' loading' : ''}`}
+    onClick={handleClick}>
       <h2>{text}</h2>
     </div>
   )
