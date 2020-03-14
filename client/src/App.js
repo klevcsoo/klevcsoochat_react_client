a14dc508f3793b6b69da691ea4261e575c2bc2be
klevcsoo <kevelajsz@outlook.com>
2020-03-14 21:20:33 +0100

    Added chatroom settings page

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import NotFoundPage from './pages/NotFoundPage'
 import ChatroomPage from './pages/ChatroomPage/ChatroomPage'
 import UserSettingsPage from './pages/UserSettingsPage/UserSettingsPage'
 import CreateChatroomPage from './pages/CreateChatroomPage/CreateChatroomPage'
+import ChatroomSettingsPage from './pages/ChatroomSettingsPage/ChatroomSettingsPage'
 
 function App() {
   return (
@@ -16,8 +17,9 @@ function App() {
         <Switch>
           <Route exact path={routes.HOME} component={HomePage} />
           <Route path={routes.USER_SETTINGS} component={UserSettingsPage} />
-          <Route path={routes.CHATROOM} component={ChatroomPage} />
           <Route path={routes.CREATE_CHATROOM} component={CreateChatroomPage} />
+          <Route exact path={routes.CHATROOM} component={ChatroomPage} />
+          <Route path={routes.CHATROOM_SETTINGS} component={ChatroomSettingsPage} />
           <Route path="*" component={NotFoundPage} />
         </Switch>
       </BrowserRouter>
