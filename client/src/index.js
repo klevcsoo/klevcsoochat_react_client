1e97418e69680fa1dc5be8c46e362750c6cabaec
Keve Lajsz <56454122+klevcsoo@users.noreply.github.com>
2020-05-07 01:53:49 +0200

    Removed old files

--- client/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import { firebaseHandler } from './Utils'
-
-ReactDOM.render(<App />, document.getElementById('root'))
-firebaseHandler.initializeApp()
