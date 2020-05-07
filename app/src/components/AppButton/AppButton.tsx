20dfa6222c516e71194af527e429814ea84524f9
Keve Lajsz <56454122+klevcsoo@users.noreply.github.com>
2020-05-07 19:25:53 +0200

    Smaller improvements

--- app/src/components/AppButton/AppButton.tsx
+++ app/src/components/AppButton/AppButton.tsx
@@ -3,15 +3,15 @@ import './AppButton.css';
 
 const AppButton = (props: {
   text: string,
-  loading: boolean,
+  loading?: boolean,
   type: 'primary' | 'secondary' | 'warning',
   onClick: () => void;
 }) => {
   const handler = () => setTimeout(() => props.onClick(), 200);
   return (
-    <button type="button" className={`app-button ${props.type}`} disabled={props.loading}
+    <button type="button" className={`app-button ${props.type}`} disabled={!!props.loading}
       onClick={handler}>
-      <div>{props.text}</div>
+      <div className="app-small-header">{props.text}</div>
     </button>
   );
 };
