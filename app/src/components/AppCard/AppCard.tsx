c7145e0c6bd625baf808804a36611efce74f09c1
Keve Lajsz <56454122+klevcsoo@users.noreply.github.com>
2020-05-07 07:12:25 +0200

    Added className prop to AddCard

--- app/src/components/AppCard/AppCard.tsx
+++ app/src/components/AppCard/AppCard.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './AppCard.css';
 
-const AppCard = (props: { children: React.ReactNode; }) => {
+const AppCard = (props: { children: React.ReactNode; className?: string; }) => {
   return (
-    <div className="app-card">
+    // Just to avoid an empty space at the end of the class string
+    <div className={`app-card${props.className ? ` ${props.className}` : ''}`}>
       {props.children}
     </div>
   );
