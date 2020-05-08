c27e7440fdc6e5cd7edf71c2fe0515adb63453ae
Keve Lajsz <56454122+klevcsoo@users.noreply.github.com>
2020-05-08 18:38:59 +0200

    Added loading prop

--- app/src/components/AppButton/AppBottomAttachedButton.tsx
+++ app/src/components/AppButton/AppBottomAttachedButton.tsx
@@ -3,12 +3,13 @@ import AppButton from './AppButton';
 
 const AppBottomAttachedButton = (props: {
   text: string,
+  loading?: boolean;
   onClick: () => void;
 }) => {
   return (
     <React.Fragment>
       <div className="app-bottom-attached-button">
-        <AppButton text={props.text} onClick={props.onClick} type="primary" />
+        <AppButton text={props.text} onClick={props.onClick} type="primary" loading={props.loading} />
       </div>
       <div style={{ height: 90 }}></div>
     </React.Fragment>
