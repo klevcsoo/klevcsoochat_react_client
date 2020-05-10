00847b46e322085b1a145d2e6abb022b4da9241e
Keve Lajsz <56454122+klevcsoo@users.noreply.github.com>
2020-05-10 19:40:25 +0200

    Moved regexes to contants

--- app/src/utils/constants.ts
+++ app/src/utils/constants.ts
@@ -5,3 +5,9 @@ export const routes = {
   CHATROOM: '/chatroom/:id',
   CHATROOM_SETTINGS: '/chatroom/:id/settings'
 };
+
+export const regex = {
+  WHITESPACE: /^\s+$/g,
+  EMAIL: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+  CHATROOM_INVITECODE: /([a-z]|[A-Z]){5,12}/g
+};
