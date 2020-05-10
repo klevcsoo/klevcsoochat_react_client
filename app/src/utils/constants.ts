6d157a45e024edfc3bacbae03c48d16f1d922861
Keve Lajsz <56454122+klevcsoo@users.noreply.github.com>
2020-05-11 00:07:55 +0200

    Added URL regex

--- app/src/utils/constants.ts
+++ app/src/utils/constants.ts
@@ -9,5 +9,6 @@ export const routes = {
 export const regex = {
   WHITESPACE: /^\s+$/g,
   EMAIL: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g,
-  CHATROOM_INVITECODE: /([a-z]|[A-Z]){5,12}/g
+  CHATROOM_INVITECODE: /([a-z]|[A-Z]){5,12}/g,
+  URL: /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/g
 };
