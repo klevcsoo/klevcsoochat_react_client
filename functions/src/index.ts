9b06301317a183869760dc9ab79e4c1c76e1e911
Keve Lajsz <56454122+klevcsoo@users.noreply.github.com>
2020-05-13 01:15:12 +0200

    Added initial functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,79 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import { defaultAccountPhoto, defaultChatroomPhoto } from './utils';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const serviceAccount = require('../service-account.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
+});
+
+exports.onUserCreate = functions.auth.user().onCreate(async (user) => {
+  await admin.auth().updateUser(user.uid, {
+    photoURL: defaultAccountPhoto
+  });
+
+  await admin.database().ref(`/users/${user.uid}/info`).set({
+    photo: user.photoURL,
+    username: user.displayName,
+    email: user.email,
+  });
+});
+
+exports.createChatroom = functions.https.onCall(async (data, context) => {
+  if (!context.auth) throw Error('Be kell jelentkezni, a szoba létrehozásához');
+
+  if (!data || !data.code || !data.name) {
+    throw Error('Nincs szoba adatok nélkül🤷‍♀️');
+  }
+
+  const uid = context.auth.uid;
+  const pushId = await admin.database().ref('/chats').push();
+  const photo = !!data.photo ? data.photo : defaultChatroomPhoto;
+  await pushId.set({
+    metadata: {
+      created: admin.database.ServerValue.TIMESTAMP,
+      creator: uid,
+      code: data.code,
+      name: data.name,
+      photo: photo
+    }
+  });
+
+  if (!pushId.key) throw Error('Hiba történt, próbáld újra');
+  return pushId.key;
+});
+
+exports.onUserChatroomsChange = functions.database.ref('/users/{user_id}/chatrooms')
+  .onWrite(async ({ before, after }, context) => {
+    const uid = context.params.user_id;
+
+    // Added new chatroom
+    if (!before.exists() && after.exists()) {
+      return admin.database().ref(`/chats/${after.key}/members/${uid}`).set(
+        admin.database.ServerValue.TIMESTAMP
+      );
+    }
+
+    // Removed a chatroom
+    if (before.exists() && !after.exists()) {
+      return admin.database().ref(`/chats/${after.key}/members/${uid}`).remove();
+    }
+  });
+
+exports.onUserRequestsChange = functions.database.ref('/users/{user_id}/requests')
+  .onWrite(async ({ before, after }, context) => {
+    const uid = context.params.user_id;
+
+    // Added new request
+    if (!before.exists() && after.exists()) {
+      return admin.database().ref(`/chats/${after.key}/metadata/requests/${uid}`).set(
+        admin.database.ServerValue.TIMESTAMP
+      );
+    }
+
+    // Removed a request
+    if (before.exists() && !after.exists()) {
+      return admin.database().ref(`/chats/${after.key}/metadata/requests/${uid}`).remove();
+    }
+  });
---
+++ functions/src/utils.ts
@@ -0,0 +1,2 @@
+export const defaultAccountPhoto = 'https://firebasestorage.googleapis.com/v0/b/klevcsoochat.appspot.com/o/default%2FDefaultAccountPhoto.png?alt=media&token=8a88aefe-debd-4799-85e4-5f78fa157176';
+export const defaultChatroomPhoto = 'https://firebasestorage.googleapis.com/v0/b/klevcsoochat.appspot.com/o/default%2FDefaultChatroomPhoto.png?alt=media&token=e68ac570-bb97-4ae1-85e8-f3d048feb4b4';
