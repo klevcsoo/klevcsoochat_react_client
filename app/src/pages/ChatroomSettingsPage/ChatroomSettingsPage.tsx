7e84d2e618cbc9ad773004c3cf149261628b9723
Keve Lajsz <56454122+klevcsoo@users.noreply.github.com>
2020-05-14 15:17:02 +0200

    Added member and request lists

--- app/src/pages/ChatroomSettingsPage/ChatroomSettingsPage.tsx
+++ app/src/pages/ChatroomSettingsPage/ChatroomSettingsPage.tsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './ChatroomSettingsPage.css';
 import { useRouteMatch } from 'react-router-dom';
-import { useChatroomMetadata } from '../../utils/firebase';
+import { useChatroomMetadata, onChatroomMember, onChatroomRequest } from '../../utils/firebase';
 import LoadingOverlay from '../../components/LoadingOverlay';
 import AppPageHeader from '../../components/AppPageHeader/AppPageHeader';
 import AppCard from '../../components/AppCard/AppCard';
 import AppInput from '../../components/AppInput/AppInput';
 import AppButton from '../../components/AppButton/AppButton';
+import AppUserCard from '../../components/AppCard/AppUserCard';
+import AppRequestCard from '../../components/AppCard/AppRequestCard';
 
 const ChatroomSettingsPage = () => {
   const roomId = (useRouteMatch().params as any).id;
@@ -14,21 +16,38 @@ const ChatroomSettingsPage = () => {
 
   const [ roomName, setRoomName ] = useState('');
   const [ updatingName, setUpdatingName ] = useState(false);
+  const [ members, setMembers ] = useState<string[]>([]);
+  const [ requests, setRequests ] = useState<string[] | null>([]);
 
   useEffect(() => {
     if (!!metadata?.name) setRoomName(metadata.name);
   }, [ metadata ]);
+
+  useEffect(() => {
+    const membersCleanUp = onChatroomMember(roomId, ((uids) => setMembers(uids)));
+    const requestsCleanUp = onChatroomRequest(roomId, ((uids) => setRequests(uids)));
+    return () => { membersCleanUp(); requestsCleanUp(); };
+  }, [ roomId ]);
 
   return metadataLoading ? <LoadingOverlay /> : !metadata ? null : (
     <div className="chatroomsettingspage-container">
       <AppPageHeader title="Szoba beállításai" previous={{ icon: 'chat' }} />
       <p style={{ fontWeight: 300 }}>
         <b>Szoba azonosítója:</b> {roomId} <br />
-        <b>Szoba neve:</b> {metadata.name} <br />
         {!metadata.code ? null : (
-          <React.Fragment><b>Meghívó kód:</b> {metadata.code}</React.Fragment>
+          <React.Fragment><b>Szoba kód:</b> {metadata.code}</React.Fragment>
         )}
       </p>
+      <div className="chatroomsettingspage-list">
+        <h2 className="app-small-header">Tagok:</h2>
+        {members.map((m, i) => <AppUserCard uid={m} key={i} reducedMargin />)}
+      </div>
+      {!requests ? null : (
+        <div className="chatroomsettingspage-list">
+          <h2 className="app-small-header">Csatlakozási kérelmek:</h2>
+          {requests.map((r, i) => <AppRequestCard uid={r} rid={roomId} reducedMargin key={i} />)}
+        </div>
+      )}
       <AppCard>
         <AppInput placeholder="Szoba neve" text={String(roomName)} onTextChanged={(text) => setRoomName(text)} />
         <AppButton text="Név módosítása" type="primary" onClick={() => {
