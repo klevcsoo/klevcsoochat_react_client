4b481dd75b5d12445b2dab0761a25287b04b466b
Keve Lajsz <56454122+klevcsoo@users.noreply.github.com>
2020-05-14 15:15:30 +0200

    Added AppRequestCard

---
+++ app/src/components/AppCard/AppRequestCard.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import './AppCard.css';
+import { useOtherUserInfo, respondToRequest } from '../../utils/firebase';
+import LoadingSpinner from '../LoadingSpinner';
+import { getOnlineStatusText } from '../../utils/functions';
+import AppButton from '../AppButton/AppButton';
+
+const AppRequestCard = (props: { uid: string, rid: string, reducedMargin?: boolean; }) => {
+  const [ user, userLoading ] = useOtherUserInfo(props.uid);
+  const [ responding, setResponding ] = useState(false);
+
+  return !userLoading && !user ? null : (
+    <div className="app-card" style={{
+      margin: `${props.reducedMargin ? '10' : '30'}px auto`
+    }}>
+      {userLoading ? <LoadingSpinner /> : !user ? null : (
+        <React.Fragment>
+          <div className="app-user-card">
+            <img src={user.photo} alt={user.username} className={user.online ? 'online' : 'offline'} />
+            <h2 className="app-small-header">{user.username ? user.username : user.email}</h2>
+            <h3>{getOnlineStatusText(user.online, user.lastOnline)}</h3>
+          </div>
+          <AppButton text="Elfogadás" type="secondary" onClick={() => {
+            setResponding(true);
+            respondToRequest(true, props.uid, props.rid).then(() => setResponding(false));
+          }} loading={responding} />
+          <AppButton text="Elutasítás" type="warning" onClick={() => {
+            setResponding(true);
+            respondToRequest(false, props.uid, props.rid).then(() => setResponding(false));
+          }} loading={responding} />
+        </React.Fragment>
+      )}
+    </div>
+  );
+};
+
+export default AppRequestCard;
