dab00ec84fea1022f5a8715b4ce5f11510acbf21
Keve Lajsz <56454122+klevcsoo@users.noreply.github.com>
2020-05-31 18:46:06 +0200

    Fixed notification animation flickering

--- app/src/components/AppNotification/AppNotifiaction.tsx
+++ app/src/components/AppNotification/AppNotifiaction.tsx
@@ -8,11 +8,12 @@ const AppNotifiaction = (props: {
   text: string,
   type: 'info' | 'error',
   visible: boolean,
-  persistent: boolean;
-  onDismiss: () => void;
+  persistent: boolean,
+  onDismiss: () => void,
+  anim: boolean;
 }) => {
   return (
-    <div className={`app-notification${props.visible ? ' visible' : ''} ${props.type}`}>
+    <div className={`app-notification${props.visible ? ' visible' : ''}${props.anim ? ' anim' : ''} ${props.type}`}>
       <AppCard>
         <h2 className="app-small-header">{props.text}</h2>
         {!props.persistent ? null : (
@@ -27,23 +28,26 @@ export function useAppNotification(props: AppNotificatonData): [ () => (JSX.Elem
   const [ visible, setVisible ] = useState(false);
   const [ render, setRender ] = useState(false);
   const [ text, setText ] = useState("");
+  const [ anim, setAnim ] = useState(true);
 
   const notification = () => (
     <React.Fragment>
       {!render ? null : (
         <AppNotifiaction text={text} type={props.type} visible={visible} onDismiss={() => {
           hide(); if (!!props.onDismiss) props.onDismiss();
-        }} persistent={props.persistent} />
+        }} persistent={props.persistent} anim={anim} />
       )}
     </React.Fragment>
   );
 
   const show = (text: string) => {
-    setText(text); setRender(true); setVisible(true);
+    setText(text); setAnim(true); setRender(true); setVisible(true);
+    setTimeout(() => setAnim(false), 600);
     if (!props.persistent && !visible) setTimeout(() => hide(), 4000);
   };
   const hide = () => {
-    setVisible(false); setTimeout(() => { setRender(false); setText(""); }, 500);
+    setAnim(true); setVisible(false);
+    setTimeout(() => { setRender(false); setText(""); }, 500);
   };
 
   return [ notification, show ];
