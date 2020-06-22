203f0e91446fa7821ed6ce63136b3a67e13b3bd6
Keve Lajsz <kevelajsz@icloud.com>
2020-06-22 23:41:49 +0200

    Changed default account and chatroom icons

--- app/src/utils/constants.ts
+++ app/src/utils/constants.ts
@@ -14,6 +14,6 @@ export const regex = {
   URL: /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/g
 };
 
-export const defaultChatroomPhoto = 'https://firebasestorage.googleapis.com/v0/b/klevcsoochat.appspot.com/o/default%2FDefaultChatroomPhoto.png?alt=media&token=e68ac570-bb97-4ae1-85e8-f3d048feb4b4';
+export const defaultChatroomPhoto = 'https://firebasestorage.googleapis.com/v0/b/klevcsoochat.appspot.com/o/default%2FDefaultChatroomPhoto.png?alt=media&token=0dace237-88fd-4ece-a2f9-540cf01c11d7';
 
 export const targetResolution = [ 375, 812 ];
--- functions/src/utils.ts
+++ functions/src/utils.ts
@@ -1,2 +1,2 @@
-export const defaultAccountPhoto = 'https://firebasestorage.googleapis.com/v0/b/klevcsoochat.appspot.com/o/default%2FDefaultAccountPhoto.png?alt=media&token=8a88aefe-debd-4799-85e4-5f78fa157176';
-export const defaultChatroomPhoto = 'https://firebasestorage.googleapis.com/v0/b/klevcsoochat.appspot.com/o/default%2FDefaultChatroomPhoto.png?alt=media&token=e68ac570-bb97-4ae1-85e8-f3d048feb4b4';
+export const defaultAccountPhoto = 'https://firebasestorage.googleapis.com/v0/b/klevcsoochat.appspot.com/o/default%2FDefaultAccountPhoto.png?alt=media&token=8de61378-711d-4dd4-8485-7fbcb7b5e0da';
+export const defaultChatroomPhoto = 'https://firebasestorage.googleapis.com/v0/b/klevcsoochat.appspot.com/o/default%2FDefaultChatroomPhoto.png?alt=media&token=0dace237-88fd-4ece-a2f9-540cf01c11d7';
