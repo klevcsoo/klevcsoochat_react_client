3f7fea704aea31e1d4d6b6cec62028eaeeedf4a5
Keve Lajsz <kevelajsz@icloud.com>
2020-07-11 02:25:28 +0200

    Added chat image uploading

--- app/src/pages/ChatroomPage/ChatroomMessageBar.tsx
+++ app/src/pages/ChatroomPage/ChatroomMessageBar.tsx
@@ -1,16 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import './ChatroomPage.css';
-import { SendRounded, InsertPhotoRounded } from '@material-ui/icons';
+import { SendRounded, InsertPhotoRounded, LinkRounded } from '@material-ui/icons';
 import AppInput from '../../components/AppInput/AppInput';
-import { sendChatMessage, setTypingStatus, onMemberTyping } from '../../utils/firebase';
+import { sendChatMessage, setTypingStatus, onMemberTyping, uploadChatImage } from '../../utils/firebase';
 import ChatroomMemberTyping from './ChatroomMemberTyping';
+import { useAppNotification } from '../../components/AppNotification/AppNotifiaction';
+import LoadingOverlay from '../../components/LoadingOverlay';
 
 const typingTimer: [ any ] = [ null ];
 
 const ChatroomMessageBar = (props: { roomId: string; }) => {
+  const [ UploadErrorNotification, showUploadError ] = useAppNotification({
+    persistent: false,
+    type: 'error'
+  });
   const [ message, setMessage ] = useState('');
   const [ urlMode, setUrlMode ] = useState(false);
   const [ membersTyping, setMembersTyping ] = useState<string[]>([]);
+  const [ uploadingImage, setUploadingImage ] = useState(false);
 
   const sendMessage = () => {
     sendChatMessage({
@@ -26,6 +33,29 @@ const ChatroomMessageBar = (props: { roomId: string; }) => {
     typingTimer[ 0 ] = setTimeout(() => setTypingStatus(false, props.roomId), 3000);
   };
 
+  const uploadPhoto = () => {
+    const fileUpload = document.createElement('input');
+    fileUpload.type = 'file'; fileUpload.style.display = 'none';
+
+    function onFile(this: any) {
+      const photo = this.files[ 0 ] as File;
+      if (!photo.type.startsWith('image/')) showUploadError('Helytelen fájltípus');
+
+      setUploadingImage(true);
+      uploadChatImage(photo).then((url) => {
+        sendChatMessage({ type: 'image', content: url }, props.roomId);
+        setUploadingImage(false);
+      }).catch((err) => {
+        console.error(err); showUploadError('Hiba történt a feltöltés során');
+        setUploadingImage(false);
+      });
+    }
+
+    fileUpload.addEventListener('change', onFile);
+    document.body.appendChild(fileUpload);
+    fileUpload.click();
+  };
+
   useEffect(() => {
     const cleanup = onMemberTyping(props.roomId, (uids) => setMembersTyping(uids));
     return cleanup;
@@ -33,13 +63,18 @@ const ChatroomMessageBar = (props: { roomId: string; }) => {
 
   return (
     <React.Fragment>
+      { uploadingImage ? <LoadingOverlay /> : null }
+      <UploadErrorNotification />
       <div className="chatroompage-typing-indicators">
         { membersTyping.map((m, i) => <ChatroomMemberTyping uid={ m } key={ i } />) }
       </div>
       <div style={ { height: 100 } }></div>
       <div className="chatroompage-messagebar acrylic-transparent">
-        <div className={ `messagebar-button toggle ${urlMode ? 'on' : 'off'}` } onClick={ () => setUrlMode(!urlMode) }>
+        <div className="messagebar-button click" onClick={ () => uploadPhoto() }>
           <InsertPhotoRounded color="inherit" />
+        </div>
+        <div className={ `messagebar-button toggle ${urlMode ? 'on' : 'off'}` } onClick={ () => setUrlMode(!urlMode) }>
+          <LinkRounded color="inherit" />
         </div>
         <form action="" method="post" autoComplete="off">
           <AppInput placeholder={ urlMode ? 'Kép link' : 'Aa' } text={ message }
