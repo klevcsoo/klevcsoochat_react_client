015533dcc093a465e45ec37183308034c865cb6f
Keve Lajsz <56454122+klevcsoo@users.noreply.github.com>
2020-09-15 11:45:49 +0200

    Limited messages to last 100

--- app/src/utils/firebase.ts
+++ app/src/utils/firebase.ts
@@ -198,7 +198,7 @@ export function onNewMessage(roomId: string, callback: (message: ChatMessage) =>
   const cached = getCachedMessages(roomId);
   if (!!cached) for (const mid of Object.keys(cached)) callback(cached[ mid ]);
 
-  const ref = app.database().ref(`/chats/${ roomId }/messages`);
+  const ref = app.database().ref(`/chats/${ roomId }/messages`).limitToLast(100);
   const handler = (snapshot: app.database.DataSnapshot) => {
     const m = snapshot.val() as ChatMessage;
     if (!!cached && !!cached[ String(snapshot.key) ]) return;
